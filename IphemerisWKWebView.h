be4ec335344b202bb7dedb6e217632ba38d0df1f
Clifford Ribaudo <cr@iphemeris.com>
2021-04-18 06:46:54 -0400

    Update IphemerisWKWebView.h

--- IphemerisWKWebView.h
+++ IphemerisWKWebView.h
@@ -10,23 +10,16 @@
 #import "HTMLPage.h"
 
 @interface IphemerisWKWebView : WKWebView
-
-@property (nonatomic, assign) BOOL chartReport;
-@property (nonatomic, assign) BOOL ephemerisPage;
-@property (nonatomic, assign) BOOL calendarPage;
+  
 @property (nonatomic, assign) CGPoint currentScrollXY;
 @property (nonatomic, assign) CGFloat currentZoomScale;
 @property (nonatomic, assign) CGRect currentContentOffset;
-@property (nonatomic, nullable, strong) HTMLPage *html;
 
 //  So that User can provide block to do something when printing starts or stops.
-//  SkyNow uses it to stop/start timer when WebView starts/finishes printing
 @property (strong) void (^ _Nullable startingPrintingAction)(void);
 @property (strong) void (^ _Nullable donePrintingAction)(void);
 
 -(void)print:(id _Nullable)sender;
--(void)printChart;
--(void)printEphemeris;
 
 #ifdef _IOS_
 -(void)snapShotImageWithCompletionHandler:(void (^_Nonnull)(UIImage * _Nullable img))completionHandler;
